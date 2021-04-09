51058df225ba4c43b05262819908e721b4f8a7f6
r-kojima <55544166+r-kojima@users.noreply.github.com>
2021-04-09 18:32:02 +0900

    セクション6.46 再レンダリングと副作用を知る(useEffect)

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   const onClickCountUp = () => {
     setNum((preNum) => preNum + 1);
   };
-  const [num, setNum] = useState(0);
+
+  const onClickSwicthShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num % 3 === 0) {
+      faceShowFlag || setFaceShowFlag(true);
+    } else {
+      faceShowFlag && setFaceShowFlag(false);
+    }
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <button onClick={onClickSwicthShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>🙃</p>}
     </>
   );
 };
